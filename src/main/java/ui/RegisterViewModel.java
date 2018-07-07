44b582598084102a69a07687ca189a33e1a43bb5
Alejandro <aleperaltabazas@hotmail.com>
2018-07-07 17:49:56 -0300

    agrego registrar que abre ventana pero no registra bien

--- src/main/java/ui/RegisterViewModel.java
+++ src/main/java/ui/RegisterViewModel.java
@@ -1,20 +1,100 @@
 package ui;
 
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 import org.uqbar.commons.model.annotations.Observable;
 
+import domain.Alumno;
+import domain.RepositorioAlumnos;
 import domain.User;
 
 @Observable
 public class RegisterViewModel {
 	String username;
+	String password;
 	MessageDigest hashed;
 	User user;
-	
-	public RegisterViewModel(User user){
-		this.username = user.getUsername();
-		this.hashed = user.getHashed();
+
+	String nombreAlumno;
+	long legajoAlumno;
+	String gitAlumno;
+
+	public RegisterViewModel(User user) {
 		this.user = user;
+	}
+
+	public void crearAlumno() {
+		this.validarElementos();
+		Alumno nuevoAlumno = new Alumno(this.getNombreAlumno(), this.getLegajoAlumno(), this.getGitAlumno(),
+				Arrays.asList());
+
+		nuevoAlumno.setHashed(this.hashed);
+		nuevoAlumno.setUsername(this.username);
+
+		RepositorioAlumnos.instancia.agregarAlumno(nuevoAlumno);
+	}
+
+	public void validarElementos() {
+		if (nombreAlumno.equals(null) || legajoAlumno < 0 || gitAlumno.equals(null) || password.equals(null)
+				|| username.equals(null)) {
+			throw new RuntimeException("Error en la creación del alumno");
+		}
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNombreAlumno() {
+		return nombreAlumno;
+	}
+
+	public void setNombreAlumno(String nombreAlumno) {
+		this.nombreAlumno = nombreAlumno;
+	}
+
+	public long getLegajoAlumno() {
+		return legajoAlumno;
+	}
+
+	public void setLegajoAlumno(long legajoAlumno) {
+		this.legajoAlumno = legajoAlumno;
+	}
+
+	public String getGitAlumno() {
+		return gitAlumno;
+	}
+
+	public void setGitAlumno(String gitAlumno) {
+		this.gitAlumno = gitAlumno;
+	}
+
+	public void hash() {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(this.getPassword().getBytes(), 0, this.getPassword().length());
+			md.digest();
+			this.setHashed(md);
+		} catch (NoSuchAlgorithmException except) {
+			except.printStackTrace();
+		}
+	}
+
+	public void setHashed(MessageDigest md) {
+		this.hashed = md;
 	}
 }
--- src/main/java/ui/RegistrarWindow.java
+++ src/main/java/ui/RegistrarWindow.java
@@ -1,22 +1,42 @@
 package ui;
 
+import org.uqbar.arena.widgets.Button;
 import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.widgets.PasswordField;
+import org.uqbar.arena.widgets.TextBox;
 import org.uqbar.arena.windows.Dialog;
 import org.uqbar.arena.windows.WindowOwner;
 
 import domain.User;
 
-public class RegistrarWindow extends Dialog<RegisterViewModel>{
+public class RegistrarWindow extends Dialog<RegisterViewModel> {
 
 	public RegistrarWindow(WindowOwner owner, User user) {
 		super(owner, new RegisterViewModel(user));
-		
+
 	}
 
 	@Override
 	protected void createFormPanel(Panel mainPanel) {
-		// TODO Auto-generated method stub
+		TextBox nombre_box = new TextBox(mainPanel);
+		TextBox legajo_box = new TextBox(mainPanel);
+		TextBox git_box = new TextBox(mainPanel);
+
+		TextBox userName = new TextBox(mainPanel);
+		PasswordField pwBox = new PasswordField(mainPanel);
 		
+		nombre_box.bindValueToProperty("nombreAlumno");
+		legajo_box.bindValueToProperty("legajoAlumno");
+		git_box.bindValueToProperty("gitAlumno");
+		
+		userName.bindValueToProperty("username");
+		pwBox.bindValueToProperty("password");
+		
+		Button okButton = new Button(mainPanel).setCaption("Ok").onClick(this::registrar);
 	}
 
+	public void registrar(){
+		this.getModelObject().crearAlumno();
+		this.close();
+	}
 }
